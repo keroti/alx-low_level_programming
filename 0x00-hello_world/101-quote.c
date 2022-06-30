8cb14ef92ba84a16ef4be9db8970a758b2437815
keroti <peterkeroti55@gmail.com>
2022-06-30 16:37:09 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - print the string in the put function
@@ -10,6 +11,6 @@
  */
 int main(void)
 {
-	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
