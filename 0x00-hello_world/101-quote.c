c06f8626323f0639ba4c52424c6ea729b5fb5d3b
keroti <peterkeroti55@gmail.com>
2022-06-30 16:39:34 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,8 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
+ * this program prints "and that piece
+ *of art is useful\" - Dora Korpar, 2015-10-19"
  * followed by a new line, to the standard error.
  * Return: 1
  */
