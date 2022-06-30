6718ef057de5dba3e3405fa8c033a301e20180f5
keroti <peterkeroti55@gmail.com>
2022-06-30 15:55:49 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,7 +4,7 @@
  * main - print the string in the put function
  *
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle
+ * this program prints sizeof various types
  * Return: 0
  */
 int main(void)
@@ -15,8 +15,8 @@ int main(void)
 	long long int p;
 	float f;
 
-	printf("size of char: %lu byte(s)\n", sizeof(c));
-	printf("Size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of char: %u byte(s)\n", sizeof(c));
+	printf("Size of an int: %u byte(s)\n", sizeof(i));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(l));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(p));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f))
