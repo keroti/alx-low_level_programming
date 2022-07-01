237be4d2c5642ef8993104e9effbf3a56196f8f1
keroti <peterkeroti55@gmail.com>
2022-07-01 08:51:31 -0700

    Alphbetical order both cases

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets using putchar function
+ *
+ * Description: using the main funtion
+ * this program print alphabets in both cases
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
