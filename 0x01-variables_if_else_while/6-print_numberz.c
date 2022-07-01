be0b098d4c6bee2924a4d61b6af12fae5f2372ec
keroti <peterkeroti55@gmail.com>
2022-07-01 09:21:24 -0700

    numbers2

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers using utcharf function
+ *
+ * Description: using the main funtion
+ * this program prints numbers from 0-9
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar("\n");
+	return (0);
+}
