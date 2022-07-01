5e342a39f37530507156899ca99650a580ba28d3
keroti <peterkeroti55@gmail.com>
2022-07-01 09:08:23 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers using printf function
+ *
+ * Description: using the main funtion
+ * this program prints numbers from 0-9
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf(i);
+	}
+	printf('\n');
+	return (0);
+}
