56e6bebd793869cc530a3e175e941dab57270510
keroti <peterkeroti55@gmail.com>
2022-07-01 08:59:15 -0700

    Alphbets except some

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets using putchar function
+ *
+ * Description: using the main funtion
+ * this program print alphabets except 'q' 'e' in lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (char != 'e' && char != 'q')
+			putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
