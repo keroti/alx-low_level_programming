1c62bf316191cf76ef2ab335f5b2e413aa057e09
keroti <peterkeroti55@gmail.com>
2022-07-05 14:24:21 -0700

    lower/uppercase

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabet
+ * @c: c is an ascii character
+ *
+ * Return: 1 if lowercase
+ */
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
