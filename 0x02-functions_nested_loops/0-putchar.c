0231d00031b8c0d128848c7e0ce042490e5de1f8
keroti <peterkeroti55@gmail.com>
2022-07-05 13:13:53 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: the putchar funtion prints "_putchar"
+ *
+ * Return: 0
+ */
+
+int _putchar()
+{
+	putchar("_putchar\n");
+	return (0);
+}
