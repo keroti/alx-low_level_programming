79ed089267d7373663dd8bd79c6ae3f29652e791
keroti <peterkeroti55@gmail.com>
2022-07-07 12:41:04 -0700

    count 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,18 @@
 #include "main.h"
 
-/*
- * print_numbers - prints numbers from 0-9
+/**
+ * print_numbers - prints numbers 0-9
  *
- * return: void
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	int i;
+	int c;
 
-	for (i = '0'; i < '10'; i++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
-	_putchar("\n");
-}	
+	_putchar('\n');
+}
