91ac4ec46d27fd27d370845ce7577a3396d8b886
keroti <peterkeroti55@gmail.com>
2022-07-07 12:40:05 -0700

    count 0-9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,18 +3,16 @@
 /*
  * print_numbers - prints numbers from 0-9
  *
- * return: 0
+ * return: void
  */
 
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; i < 10; i++)
+	for (i = '0'; i < '10'; i++)
 	{
 		_putchar(i);
 	}
 	_putchar("\n");
-
-	return (0);
 }	
