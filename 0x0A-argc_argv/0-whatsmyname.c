cf744650f4012c2d8b84b4c8a20c2096e7bea419
keroti <peterkeroti55@gmail.com>
2022-07-22 05:19:52 -0700

    First commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - prints value of argv[0]
+ * @argc: integer
+ * @argv[]: array with size of argc
+ *
+ * Return: 0
+ */
+
+int main(int argc, char* argv[])
+{
+	_putchar("%s", argv[0]);
+	return 0;
+}
